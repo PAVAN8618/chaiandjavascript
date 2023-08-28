9741ce73ce04a8d52a5682ce0e826345f64a0880
Pavan Kumar Tiwari <55940294+PAVAN8618@users.noreply.github.com>
2023-08-28 10:17:47 +0000

    maps

--- jsadvance/firstad.js
+++ jsadvance/firstad.js
@@ -44,4 +44,55 @@ function myFunction(y,x) {
 function myFunction(x, y = 10) {//y have default values if we pass the value in the then value will taken if it is not not passed then i will take default values in the script. 
     return x + y;
   }
- console.log(myFunction(5,22));
+//  console.log(myFunction(5,22));
+
+
+function sum(...args) {
+    let sums = 0;
+
+    for (const iterator of args) {
+        sums +=iterator;
+        return sums;
+    }
+  }
+  
+  let x = sum(4, 9, 16, 25, 29, 100, 66, 77);
+
+//   console.log(x); 
+
+//................ Set ......................//
+const sets = new Set();
+
+sets.add("11")
+sets.add("11")
+sets.add("11")
+sets.add("13")
+sets.add("11")
+sets.add("15")
+
+//console.log(sets.values());
+let mapss= sets.entries()
+for (const iterator of mapss) {
+  //console.log(iterator);
+}
+
+
+///map.......
+
+const fruits = new Map([
+  ["apples", 500],
+  ["bananas", 300],
+  ["oranges", 200]
+]);
+
+console.log(fruits);
+
+fruits.set("apples",400);
+// console.log(fruits)
+
+// console.log(fruits.values())
+
+// console.log(fruits.keys() , fruits.values());
+
+
+fruits.forEach((key, value)=> console.log(key, value))
