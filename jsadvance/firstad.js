840370bb76e1dccf39eafbf791d835b87cc4b8f3
Pavan Kumar Tiwari <55940294+PAVAN8618@users.noreply.github.com>
2023-08-28 07:12:09 +0000

    2

--- jsadvance/firstad.js
+++ jsadvance/firstad.js
@@ -1,1 +1,11 @@
-console.log("apwan");
+// Immediately Invoked Function Expressions (IIFE)
+
+
+(function chai(){
+    // named IIFE
+    console.log(`DB CONNECTED`);
+})();
+
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+} )('pawan')
